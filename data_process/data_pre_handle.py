f59c57bd82d3c7f12aa73fe22b8d869902ee345b
nick.qiu <nick.qiu@sap.com>
2023-07-09 22:40:52 +0800

    update the readme

--- data_process/data_pre_handle.py
+++ data_process/data_pre_handle.py
@@ -1,4 +1,5 @@
 import jieba
+import logging
 from mongo_corpus_reader import MongodbCorpusReader
 from mongodbUtil import MongodbUtil
 
@@ -6,19 +7,27 @@ from mongodbUtil import MongodbUtil
 def remove_pre_fix_for_source_field():
     reader = MongodbCorpusReader('mongodb://nick_2014:nick_2088_21@localhost:32774/?authMechanism=DEFAULT', 'user')
     result = reader.__iter__(['_id', 'source'])
+    process_count = 0
     for field_dict in result:
         field_id = field_dict['_id']
         field_source = field_dict['source']
-        process_count = 0
         if field_source:
             field_source = field_source.replace('来自', '')
             value_fields = {'field': 'source', 'value': field_source}
             mongodbUtil = MongodbUtil()
             mongodbUtil.replace_data(reader.getCollection(), field_id, value_fields)
             process_count += 1
+    return process_count
 
-# 先把user表中的source字段的前缀去掉 如来自上海 变为上海
-remove_pre_fix_for_source_field()
+def main():
+    # 先把user表中的source字段的前缀去掉 如来自上海 变为上海
+    processed_count = remove_pre_fix_for_source_field()
+    logging.info("processed_count: %d", processed_count)
+    # 把comment collection中的content里面的内容删除‘回复‘两个字
+    # 对content进行分词、词性标记
+    # 对文本内容进行聚类
 
-
+if __name__ == '__main__':
+    main()
+    logging.info("process done")
             
